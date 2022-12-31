a324253318d5aade8adb56b3c67b8b162277eea6
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2022-12-31 12:38:16 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END"
